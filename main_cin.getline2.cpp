dcd38886a7647dce65c05ab8bfdb2f263280c73d
CarlosEduardoValleRiveiro <136513137+CarlosEduardoValleRiveiro@users.noreply.github.com>
2023-06-20 15:00:58 -0600

    Add files via upload

---
+++ main_cin.getline2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	
+	char nombre[30];
+	
+	cout<<"Escriba una frase con un maximo de 30 caracteres"<<endl;
+		
+	cin.getline(nombre,30);
+	
+    cout<<nombre[29];
+    cout<<nombre[28];
+    cout<<nombre[27];
+    cout<<nombre[26];
+    cout<<nombre[25];
+    cout<<nombre[24];
+    cout<<nombre[23];
+    cout<<nombre[22];
+    cout<<nombre[21];
+    cout<<nombre[20];
+    cout<<nombre[19];
+    cout<<nombre[18];
+    cout<<nombre[17];
+    cout<<nombre[16];
+    cout<<nombre[15];
+    cout<<nombre[14];
+    cout<<nombre[13];
+    cout<<nombre[12];
+    cout<<nombre[11];
+    cout<<nombre[10];
+    cout<<nombre[9];
+    cout<<nombre[8];
+    cout<<nombre[7];
+    cout<<nombre[6];
+    cout<<nombre[5];
+    cout<<nombre[4];
+    cout<<nombre[3];
+    cout<<nombre[2];
+    cout<<nombre[1];
+    cout<<nombre[0];
+}
